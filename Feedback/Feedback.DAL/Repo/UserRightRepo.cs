de074ebfebbdfa7a3d1660c65d4fa32526670554
sangeeta77 <sangeetatimsina1967@gmail.com>
2019-02-17 16:46:23 +0545

    right info listing done

---
+++ Feedback/Feedback.DAL/Repo/UserRightRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Feedback.DAL.Model;
+namespace Feedback.DAL.Repo
+{
+    public interface IUserRightRepo
+    {
+        List<RightInfo> GetAllRights();
+    }
+    public class UserRightRepo : IUserRightRepo
+    {
+        public List<RightInfo> GetAllRights()
+        {
+              using (FeedBackEntities _entites = new FeedBackEntities())
+            {
+                return _entites.RightInfoes.ToList();
+            }
+        }
+    }
+}
---
+++ Feedback/Feedback.Service/Service/RightInfoService.cs
@@ -0,0 +1,28 @@
+﻿using Feedback.DAL.Model;
+using Feedback.DAL.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feedback.Service.Service
+{
+    public interface IRightInfoService
+    {
+        List<RightInfo> GetAllRights();
+    }
+    public class RightInfoService : IRightInfoService
+    {
+        private IUserRightRepo _userRightRepo;
+        public RightInfoService(IUserRightRepo userRightRepo)
+        {
+            _userRightRepo = userRightRepo;
+        }
+        public List<RightInfo> GetAllRights()
+        {
+            List<RightInfo> list= _userRightRepo.GetAllRights();
+            return list;
+        }
+    }
+}
